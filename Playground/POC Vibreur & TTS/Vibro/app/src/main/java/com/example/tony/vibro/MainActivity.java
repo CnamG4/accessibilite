a0c0b0f65f2e2bfbf2576bcc72400a99950dc4db
TWickert <tony.wickert@live.fr>
2015-06-06 15:47:55 +0200

    Correction du dernier commit..
    Supression "poc TTS"
    ajout dossier "poc vibreur & TTS"
    qui contient les 2 pocs

---
+++ Playground/POC Vibreur & TTS/Vibro/app/src/main/java/com/example/tony/vibro/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.tony.vibro;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+public class MainActivity extends Activity {
+
+
+    private static final int RESULT_SPEECH = 1;
+    private final int CHECK_CODE = 0x1;
+    private final int LONG_DURATION = 5000;
+    private final int SHORT_DURATION = 1200;
+
+    VibratorService vs = new VibratorService(this);
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.btnCancel);
+
+        tts= TTSService.getTTS(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 cancelled();
+            }
+        });
+
+        Button buttonV = (Button) findViewById(R.id.btnValidate);
+        buttonV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate();
+            }
+        });
+    }
+
+    void validate(){
+        vs.validate();
+        TTSService.Speak("Anthony le FDP");
+
+
+    }
+    void cancelled(){
+        vs.cancelled();
+        TTSService.Speak("Tavernier le Noob");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ Playground/POC Vibreur & TTS/Vibro/app/src/main/java/com/example/tony/vibro/TTSService.java
@@ -0,0 +1,78 @@
+package com.example.tony.vibro;
+
+import android.annotation.TargetApi;
+import android.app.Service;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.content.Intent;
+import android.app.Activity;
+import android.util.Log;
+import java.util.Locale;
+import java.util.Random;
+
+public class TTSService extends Activity{
+
+    private static TextToSpeech tts;
+
+
+
+    public static TextToSpeech getTTS(Activity a){
+        tts = new TextToSpeech(a, new TextToSpeech.OnInitListener() {
+
+            @Override
+            public void onInit(int status) {
+                // TODO Auto-generated method stub
+                if(status == TextToSpeech.SUCCESS){
+                    int result=tts.setLanguage(Locale.FRANCE);
+                    if(result==TextToSpeech.LANG_MISSING_DATA ||
+                            result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("error", "This Language is not supported");
+                    }
+                    else{
+                        Speak("Book where you are the hero");
+                    }
+                }
+                else
+                    Log.e("error", "Initilization Failed!");
+            }
+        });
+        return tts;
+    }
+
+    public static TextToSpeech getTTS(Activity a, final Locale loc){
+        tts = new TextToSpeech(a, new TextToSpeech.OnInitListener() {
+
+            @Override
+            public void onInit(int status) {
+                // TODO Auto-generated method stub
+                if(status == TextToSpeech.SUCCESS){
+                    int result=tts.setLanguage(loc);
+                    if(result==TextToSpeech.LANG_MISSING_DATA ||
+                            result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("error", "This Language is not supported");
+                    }
+                    else{
+                        Speak("Application lancée");
+                    }
+                }
+                else
+                    Log.e("error", "Initilization Failed!");
+            }
+        });
+        return tts;
+    }
+
+    public static void Speak(String text) {
+        // TODO Auto-generated method stub
+        if(text==null||"".equals(text))
+        {
+            text = "Content not available";
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }else
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+}
